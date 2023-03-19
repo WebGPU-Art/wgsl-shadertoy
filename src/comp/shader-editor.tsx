7424ab77e95acb82cba38277b86bbefd4403018a
tiye <jiyinyiyong@gmail.com>
2023-03-19 15:54:48 +0800

    fix editor area scrolling

--- src/comp/shader-editor.tsx
+++ src/comp/shader-editor.tsx
@@ -10,7 +10,37 @@ function ShaderEditor(props: {
   let [localCode, setLocalCode] = useState(props.code);
 
   return (
-    <div className={cx(cssEditor, column)}>
+    <div className={cx(column, cssEditor)}>
+      <div className={cssEditorWrapper}>
+        <CodeEditor
+          value={localCode}
+          language="rust"
+          placeholder="Please enter JS code."
+          onChange={(evn) => {
+            setLocalCode(evn.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key == "Enter" && (event.ctrlKey || event.metaKey)) {
+              props.onChange(localCode);
+              event.preventDefault();
+            }
+
+            if (event.key == "s" && (event.ctrlKey || event.metaKey)) {
+              props.onChange(localCode);
+              event.preventDefault();
+            }
+          }}
+          padding={15}
+          style={{
+            fontSize: 12,
+            // flex: 1,
+            borderRadius: "4px",
+            overflow: "auto",
+            fontFamily:
+              "Source Code Pro,ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+          }}
+        />
+      </div>
       <div className={cx(rowMiddle, cssHeader)}>
         <button
           className={cssButton}
@@ -30,33 +60,6 @@ function ShaderEditor(props: {
           </a>
         </div>
       </div>
-      <CodeEditor
-        value={localCode}
-        language="rust"
-        placeholder="Please enter JS code."
-        onChange={(evn) => {
-          setLocalCode(evn.target.value);
-        }}
-        onKeyDown={(event) => {
-          if (event.key == "Enter" && (event.ctrlKey || event.metaKey)) {
-            props.onChange(localCode);
-            event.preventDefault();
-          }
-
-          if (event.key == "s" && (event.ctrlKey || event.metaKey)) {
-            props.onChange(localCode);
-            event.preventDefault();
-          }
-        }}
-        padding={15}
-        style={{
-          fontSize: 12,
-          flex: 1,
-          borderRadius: "4px",
-          fontFamily:
-            "Source Code Pro,ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-        }}
-      />
     </div>
   );
 }
@@ -73,7 +76,8 @@ const cssEditor = css`
   width: 40vw;
   border-radius: 8px;
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.3);
-  backgroundcolor: #fff;
+  background-color: #fff;
+  overflow: auto;
 
   &:hover {
     opacity: 0.9;
@@ -82,20 +86,33 @@ const cssEditor = css`
 `;
 
 const cssButton = css`
-  display: inline-block;
+  cursor: pointer;
 `;
 
 const cssHeader = css`
   padding: 6px;
   justify-content: flex-end;
   gap: 6px;
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  opacity: 0.5;
+  transition-duration: 200ms;
+
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const cssLink = css`
   color: #aaf;
   font-family: Helvetica Neue, Serif;
-  background-color: white;
   padding: 2px 8px;
-  border-radius: 6px;
   font-size: 12px;
+`;
+
+const cssEditorWrapper = css`
+  flex: 1;
+  overflow: auto;
+  padding-bottom: 120px;
 `;
