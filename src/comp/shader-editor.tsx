a579af7c3d5d8d13be5ac64fb03bdbafd68fa4e7
tiye <jiyinyiyong@gmail.com>
2023-03-22 02:53:45 +0800

    select based code with float tabs

--- src/comp/shader-editor.tsx
+++ src/comp/shader-editor.tsx
@@ -2,65 +2,91 @@ import { useEffect, useState } from "react";
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import { css, cx } from "@emotion/css";
 import { rowParted, expand, column, rowMiddle } from "@worktools/flex-styles";
+import FloatTabs from "./float-tabs";
 
 function ShaderEditor(props: {
   code: string;
   onChange: (code: string) => void;
 }) {
   let [localCode, setLocalCode] = useState(props.code);
+  let [title, setTitle] = useState("Base");
+
+  const [visible, setVisible] = useState(false);
 
   return (
-    <div className={cx(column, cssEditor)}>
-      <div className={cssEditorWrapper}>
-        <CodeEditor
-          value={localCode}
-          language="rust"
-          placeholder="Please enter JS code."
-          onChange={(evn) => {
-            setLocalCode(evn.target.value);
+    <>
+      {visible ? (
+        <FloatTabs
+          onSelect={(tab) => {
+            setTitle(tab.title);
+            setLocalCode(tab.code);
+            props.onChange(tab.code);
           }}
-          onKeyDown={(event) => {
-            if (event.key == "Enter" && (event.ctrlKey || event.metaKey)) {
-              props.onChange(localCode);
-              event.preventDefault();
-            }
-
-            if (event.key == "s" && (event.ctrlKey || event.metaKey)) {
-              props.onChange(localCode);
-              event.preventDefault();
-            }
-          }}
-          padding={15}
-          style={{
-            fontSize: 12,
-            // flex: 1,
-            borderRadius: "4px",
-            overflow: "auto",
-            fontFamily:
-              "Source Code Pro,ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+          onClose={() => {
+            setVisible(false);
           }}
         />
-      </div>
-      <div className={cx(rowMiddle, cssHeader)}>
-        <button
-          className={cssButton}
+      ) : null}
+      <div className={cx(column, cssEditor)}>
+        <div
           onClick={() => {
-            props.onChange(localCode);
+            setVisible(!visible);
           }}
+          className={cssTitle}
         >
-          Submit (Command+s)
-        </button>
-        <div>
-          <a
-            href="https://github.com/Triadica/wgsl-shadertoy"
-            target="_blank"
-            className={cssLink}
+          {title} ▾
+        </div>
+        <div className={cssEditorWrapper}>
+          <CodeEditor
+            value={localCode}
+            language="rust"
+            placeholder="Please enter JS code."
+            onChange={(evn) => {
+              setLocalCode(evn.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key == "Enter" && (event.ctrlKey || event.metaKey)) {
+                props.onChange(localCode);
+                event.preventDefault();
+              }
+
+              if (event.key == "s" && (event.ctrlKey || event.metaKey)) {
+                props.onChange(localCode);
+                event.preventDefault();
+              }
+            }}
+            padding={15}
+            style={{
+              fontSize: 12,
+              // flex: 1,
+              borderRadius: "4px",
+              overflow: "auto",
+              fontFamily:
+                "Source Code Pro,ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+        </div>
+        <div className={cx(rowMiddle, cssHeader)}>
+          <button
+            className={cssButton}
+            onClick={() => {
+              props.onChange(localCode);
+            }}
           >
-            GitHub
-          </a>
+            Submit (Command+s)
+          </button>
+          <div>
+            <a
+              href="https://github.com/Triadica/wgsl-shadertoy"
+              target="_blank"
+              className={cssLink}
+            >
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -115,4 +141,17 @@ const cssEditorWrapper = css`
   flex: 1;
   overflow: auto;
   padding-bottom: 120px;
+`;
+
+const cssTitle = css`
+  font-family: Hind, Helvetica Neue, Serif;
+  padding: 4px 12px;
+  color: #888;
+  font-size: 14px;
+  cursor: pointer;
+  transition-duration: 200ms;
+
+  &:hover {
+    background-color: #f8f8f8;
+  }
 `;
